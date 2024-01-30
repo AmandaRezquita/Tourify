011dae07c82c500876b97f0c0cdc45d9199751c6
AmandaRezquita <amandarezquita1969@gmail.com>
2024-01-30 21:54:39 +0700

    Style [AR]

--- lib/pages/homepage/widget/paket_listview.dart
+++ lib/pages/homepage/widget/paket_listview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tourify/Constant/Constant.dart';
 import 'package:tourify/pages/homepage/homeController.dart';
 
 class PaketListview extends StatelessWidget {
@@ -51,6 +52,64 @@ class PaketListview extends StatelessWidget {
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.white70),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Text(
+                                  controller.paketList[index]["text1"]!,
+                                  style: const TextStyle(
+                                      color: MyColors.blackFont,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w800),
+                                )),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 6),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.location_on,
+                                    size: 20,
+                                    color: Colors.black38,
+                                  ),
+                                  const SizedBox(width: 2),
+                                  Text(
+                                    controller.paketList[index]["text2"]!,
+                                    style: const TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w500),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 1),
+                                    child: Text(
+                                      controller.paketList[index]["rating"]!,
+                                      style: const TextStyle(
+                                          color: MyColors.blackFont,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Icon(
+                                    Icons.star,
+                                    size: 23,
+                                    color: Colors.yellowAccent,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     )
                   ],
